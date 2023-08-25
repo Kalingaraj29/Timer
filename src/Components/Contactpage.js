e244b1e20b1a1d71a743e1bb47774c63ab8c1ae7
Kalingaraj29 <Kalingaraj127@gmail.com>
2023-08-25 11:43:26 +0530

    added

--- src/Components/Contactpage.js
+++ src/Components/Contactpage.js
@@ -1,1 +1,12 @@
-<h1>Contact page</h1>
+import React from 'react'
+
+const Contactpage = () => {
+  return (
+    <div>
+       <h1>Cont page
+       </h1>
+    </div>
+  )
+}
+
+export default Contactpage
